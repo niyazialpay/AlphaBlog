576b26abf02f00ce70e276b272d027aae7852d83
Muhammed Niyazi Alpay <muhammed@niyazialpay.com>
2024-05-11 23:18:44 +0300

    update

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,10 +2,10 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\EventServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\GlobalVariableServiceProvider::class,
     App\Providers\HelperServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
     Illuminate\Session\SessionServiceProvider::class,
-    App\Providers\EventServiceProvider::class,
 ];
