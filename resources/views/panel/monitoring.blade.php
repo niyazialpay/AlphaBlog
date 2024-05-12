ba5b5296539a72741b8fcf09e3a8d5098a2a9371
Muhammed Niyazi Alpay <muhammed@niyazialpay.com>
2024-05-13 00:16:02 +0300

    update

--- resources/views/panel/monitoring.blade.php
+++ resources/views/panel/monitoring.blade.php
@@ -8,10 +8,10 @@
 @endsection
 @section('content')
     <div class="row">
-        <div class="col-sm-11 col-md-11 col-lg-11 container py-2 px-4">
+        <div class="col-sm-12">
             <div class="card">
                 <div class="card-body">
-                    <iframe id="monitoring" src="{{$iframe_url}}" style="width: 100%; height: calc(100dvh - 325px); border: none;"></iframe>
+                    <iframe id="monitoring" src="{{$iframe_url}}"></iframe>
                 </div>
             </div>
         </div>
