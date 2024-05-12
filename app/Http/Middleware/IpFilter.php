a5558f47749e00bdb52d891870910c29702705c4
Muhammed Niyazi Alpay <muhammed@niyazialpay.com>
2024-05-12 13:57:24 +0300

    update

--- app/Http/Middleware/IpFilter.php
+++ app/Http/Middleware/IpFilter.php
@@ -32,25 +32,39 @@ class IpFilter
         if ($filter->count() == 0) {
             $status = true;
         }
+        $blacklisted_ips = [];
+        $whitelisted_ips = [];
+        $blacklisted_route_list = [];
+        $whitelisted_route_list = [];
         foreach ($filter as $filter_item) {
-            if (IpUtils::checkIp($request->getClientIp(), $filter_item->ipList->pluck('ip')->toArray())) {
-                if ($request->is($filter_item->routeList->pluck('route')->toArray())) {
-                    if ($filter_item->list_type == 'blacklist') {
-                        $status = false;
-                    } else {
-                        $status = true;
-                        break;
-                    }
-                } else {
-                    $status = true;
-                    break;
+            //print_r($filter_item->ipList->pluck('ip')->toArray());
+            if($request->is($filter_item->routeList->pluck('route')->toArray())){
+                /*echo "Route: ".$request->path()."\n";
+                echo "Client IP: ".$request->getClientIp()."\n";
+                echo "List Type: ".$filter_item->list_type."\n";
+                if($filter_item->list_type == 'blacklist')
+                    echo "Blacklisted ";
+                else
+                    echo "Whitelisted ";
+                echo "IP List: ".implode(", ", $filter_item->ipList->pluck('ip')->toArray())."\n\n\n";*/
+
+
+                if($filter_item->list_type == 'blacklist'){
+                    $blacklisted_ips = array_merge($blacklisted_ips, $filter_item->ipList->pluck('ip')->toArray());
+                    $blacklisted_route_list = array_merge($blacklisted_route_list, $filter_item->routeList->pluck('route')->toArray());
                 }
-            } else {
+                else{
+                    $whitelisted_ips = array_merge($whitelisted_ips, $filter_item->ipList->pluck('ip')->toArray());
+                    $whitelisted_route_list = array_merge($whitelisted_route_list, $filter_item->routeList->pluck('route')->toArray());
+                }
+
+            }
+            /*if (IpUtils::checkIp($request->getClientIp(), $filter_item->ipList->pluck('ip')->toArray())) {
                 if ($request->is($filter_item->routeList->pluck('route')->toArray())) {
                     if ($filter_item->list_type == 'blacklist') {
-                        $status = true;
-                    } else {
                         $status = false;
+                    } else {
+                        $status = true;
                         break;
                     }
                 } else {
@@ -58,12 +72,41 @@ class IpFilter
                     break;
                 }
             }
+            else {
+                if ($request->is($filter_item->routeList->pluck('route')->toArray())) {
+                    if ($filter_item->list_type == 'blacklist') {
+                        $status = true;
+                    } else {
+                        $status = false;
+                        break;
+                    }
+                } else {
+                    $status = true;
+                    break;
+                }
+            }*/
         }
 
+        if(count($blacklisted_ips) > 0){
+            if (IpUtils::checkIp($request->getClientIp(), $blacklisted_ips) && $request->is(implode(',', $blacklisted_route_list))) {
+                abort(404);
+            }
+        }
+
+        if(count($whitelisted_ips) > 0){
+            if (IpUtils::checkIp($request->getClientIp(), $whitelisted_ips) && $request->is(implode(',', $whitelisted_route_list))) {
+                abort(404);
+            }
+            else{
+                return $next($request);
+            }
+        }
+        return $next($request);
+        /*print_r($blacklisted_ips);
         if ($status) {
             return $next($request);
         }
-        abort(404);
-        return $next($request);
+        //abort(404);
+        return $next($request);*/
     }
 }
