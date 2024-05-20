df25b3820336a1fd2726a451fbf1bf45151352ae
Muhammed Niyazi Alpay <muhammed@niyazialpay.com>
2024-05-20 21:47:30 +0300

    update

--- resources/lang/tr/cache.php
+++ resources/lang/tr/cache.php
@@ -4,4 +4,5 @@ return [
     'clear_cache' => 'Önbelleği Temizle',
     'cache_cleared' => 'Önbellek temizlendi.',
     'cache_not_cleared' => 'Önbellek temizlenemedi.',
+    'are_you_sure' => 'Önbelleği temizlemek istediğinize emin misiniz?',
 ];
