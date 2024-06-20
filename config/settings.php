8e70cfb32fe4262582205b382c93638ea45620b1
Muhammed Niyazi Alpay <muhammed@niyazialpay.com>
2024-06-21 00:33:32 +0300

    update

--- config/settings.php
+++ config/settings.php
@@ -3,4 +3,5 @@
 return [
     'admin_panel_path' => env('ADMIN_PANEL_PATH', 'admin'),
     'notification_send_method' => env('NOTIFICATION_SEND_METHOD', 'directly'),
+    'mail_send_method' => env('MAIL_SEND_METHOD', 'directly'),
 ];
