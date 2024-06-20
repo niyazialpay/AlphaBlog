1a3cb61b7cec71c9cf4d9077c84efb2d0e08a7c9
Muhammed Niyazi Alpay <muhammed@niyazialpay.com>
2024-06-20 21:44:50 +0300

    update

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CommentRequest;
+use App\Jobs\SendNotificationToAdmin;
 use App\Models\OneSignal;
 use App\Models\Post\Comments;
 use App\Models\Post\Posts;
+use GuzzleHttp\Exception\GuzzleException;
 
 class CommentController extends Controller
 {
+    /**
+     * @throws GuzzleException
+     */
     public function store(CommentRequest $request)
     {
         $comment = new Comments();
@@ -20,13 +25,19 @@ class CommentController extends Controller
         $comment->user_agent = $request->userAgent();
 
         if ($comment->save()) {
-            $onesignal = OneSignal::first();
-            if ($onesignal) {
-                OneSignal::sendPush([
-                    'en' => Posts::find($request->validated('post_id'))->title,
-                ], [
-                    'en' => __('comments.new_comment_notification'),
-                ]);
+            if(config('settings.notification_send_method') == 'directly'){
+                $onesignal = OneSignal::first();
+                if ($onesignal) {
+                    OneSignal::sendPush(
+                        Posts::find($request->validated('post_id'))->title,
+                        __('comments.new_comment_notification'),
+                        route('admin.post.edit', $request->validated('post_id')),
+                        5
+                    );
+                }
+            }
+            else{
+                SendNotificationToAdmin::dispatch($request->validated('post_id'));
             }
 
             return response()->json([
--- config/settings.php
+++ config/settings.php
@@ -2,4 +2,5 @@
 
 return [
     'admin_panel_path' => env('ADMIN_PANEL_PATH', 'admin'),
+    'notification_send_method' => env('NOTIFICATION_SEND_METHOD', 'directly'),
 ];
