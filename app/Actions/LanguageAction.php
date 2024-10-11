7608108648f0889cbf96509de79410ede7217768
Muhammed Niyazi Alpay <muhammed@niyazialpay.com>
2024-10-11 20:17:37 +0300

    update

--- app/Actions/LanguageAction.php
+++ app/Actions/LanguageAction.php
@@ -8,69 +8,71 @@ class LanguageAction
 {
     public static function setLanguage($request): void
     {
-        $except = [
-            '_debugbar',
-            'api',
-            'image',
-            'rss',
-            'sitemap',
-            'sitemap.xml',
-            'manifest.json',
-            config('settings.admin_panel_path'),
-            'forgot-password',
-            'reset-password',
-            'login',
-            'cdn-cgi',
-            'up',
-            'user',
-            'webauthn',
-            'pulse',
-            'livewire',
-            'email',
-            'email/verify',
-        ];
-        $languages = new Languages;
-        if (! in_array($request->segment(1), $except)) {
-            if (session()->has('language')) {
-                if ($request->segment(1) == session('language')) {
+        if(config('app.key')){
+            $except = [
+                '_debugbar',
+                'api',
+                'image',
+                'rss',
+                'sitemap',
+                'sitemap.xml',
+                'manifest.json',
+                config('settings.admin_panel_path'),
+                'forgot-password',
+                'reset-password',
+                'login',
+                'cdn-cgi',
+                'up',
+                'user',
+                'webauthn',
+                'pulse',
+                'livewire',
+                'email',
+                'email/verify',
+            ];
+            $languages = new Languages;
+            if (! in_array($request->segment(1), $except)) {
+                if (session()->has('language')) {
+                    if ($request->segment(1) == session('language')) {
+                        $language = $languages->getLanguage(session('language'));
+                    } elseif ($request->segment(1) == null) {
+                        $language = $languages->getLanguage(app('default_language')->code);
+                    } else {
+                        $language = $languages->getLanguage($request->segment(1));
+                        if ($language == null) {
+                            abort(404);
+                        }
+                    }
+                } else {
+                    if ($request->segment(1) == null) {
+                        $language = $languages->getLanguage(app('default_language')?->code);
+                    } else {
+                        $language = $languages->getLanguage($request->segment(1));
+                        if ($language == null) {
+                            abort(404);
+                        }
+                    }
+                }
+            } else {
+                if (session()->has('language')) {
                     $language = $languages->getLanguage(session('language'));
-                } elseif ($request->segment(1) == null) {
-                    $language = $languages->getLanguage(app('default_language')->code);
                 } else {
-                    $language = $languages->getLanguage($request->segment(1));
-                    if ($language == null) {
-                        abort(404);
-                    }
-                }
-            } else {
-                if ($request->segment(1) == null) {
-                    $language = $languages->getLanguage(app('default_language')?->code);
-                } else {
-                    $language = $languages->getLanguage($request->segment(1));
-                    if ($language == null) {
-                        abort(404);
+                    $language = $languages->getLanguage(
+                        explode('-', explode(',', $request->server('HTTP_ACCEPT_LANGUAGE')
+                        )[0])[0]
+                    );
+                    if (! $language) {
+                        $language = $languages->getLanguage(app('default_language')->code);
                     }
                 }
             }
-        } else {
-            if (session()->has('language')) {
-                $language = $languages->getLanguage(session('language'));
-            } else {
-                $language = $languages->getLanguage(
-                    explode('-', explode(',', $request->server('HTTP_ACCEPT_LANGUAGE')
-                    )[0])[0]
-                );
-                if (! $language) {
-                    $language = $languages->getLanguage(app('default_language')->code);
-                }
-            }
-        }
-        session()->put('language', $language?->code);
-        session()->put('language_flag', $language?->flag);
-        session()->put('language_name', $language?->name);
+            session()->put('language', $language?->code);
+            session()->put('language_flag', $language?->flag);
+            session()->put('language_name', $language?->name);
 
-        app()->setLocale($language?->code);
-        setlocale(LC_ALL, $language?->code);
-        setlocale(LC_TIME, $language?->code);
+            app()->setLocale($language?->code);
+            setlocale(LC_ALL, $language?->code);
+            setlocale(LC_TIME, $language?->code);
+        }
     }
 }
