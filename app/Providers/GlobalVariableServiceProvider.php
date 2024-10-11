b1333ba165614e0dd075302dbd9718f1bb6802a5
Muhammed Niyazi Alpay <muhammed@niyazialpay.com>
2024-10-11 20:14:42 +0300

    update

--- app/Providers/GlobalVariableServiceProvider.php
+++ app/Providers/GlobalVariableServiceProvider.php
@@ -29,101 +29,103 @@ class GlobalVariableServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (Cache::has(config('cache.prefix').'general_settings')) {
-            $general_settings = Cache::get(config('cache.prefix').'general_settings');
-        } else {
-            $general_settings = Cache::rememberForever(config('cache.prefix').'general_settings', function () {
-                return GeneralSettings::first();
+        if(config('app.key')){
+            if (Cache::has(config('cache.prefix').'general_settings')) {
+                $general_settings = Cache::get(config('cache.prefix').'general_settings');
+            } else {
+                $general_settings = Cache::rememberForever(config('cache.prefix').'general_settings', function () {
+                    return GeneralSettings::first();
+                });
+            }
+            App::singleton('general_settings', function () use ($general_settings) {
+                return $general_settings;
             });
-        }
-        App::singleton('general_settings', function () use ($general_settings) {
-            return $general_settings;
-        });
 
-        if (Cache::has(config('cache.prefix').'languages')) {
-            $languages = Cache::get(config('cache.prefix').'languages');
-        } else {
-            $languages = Cache::rememberForever(config('cache.prefix').'languages', function () {
-                return Languages::where('is_active', true)->get();
+            if (Cache::has(config('cache.prefix').'languages')) {
+                $languages = Cache::get(config('cache.prefix').'languages');
+            } else {
+                $languages = Cache::rememberForever(config('cache.prefix').'languages', function () {
+                    return Languages::where('is_active', true)->get();
+                });
+            }
+            App::singleton('languages', function () use ($languages) {
+                return $languages;
             });
-        }
-        App::singleton('languages', function () use ($languages) {
-            return $languages;
-        });
 
-        if (Cache::has(config('cache.prefix').'default_language')) {
-            $default_language = Cache::get(config('cache.prefix').'default_language');
-        } else {
-            $default_language = Cache::rememberForever(config('cache.prefix').'default_language', function () {
-                return Languages::where('is_default', true)->first();
+            if (Cache::has(config('cache.prefix').'default_language')) {
+                $default_language = Cache::get(config('cache.prefix').'default_language');
+            } else {
+                $default_language = Cache::rememberForever(config('cache.prefix').'default_language', function () {
+                    return Languages::where('is_default', true)->first();
+                });
+            }
+            App::singleton('default_language', function () use ($default_language) {
+                return $default_language;
             });
-        }
-        App::singleton('default_language', function () use ($default_language) {
-            return $default_language;
-        });
 
-        if (Cache::has(config('cache.prefix').'theme')) {
-            $theme = Cache::get(config('cache.prefix').'theme');
-        } else {
-            $theme = Cache::rememberForever(config('cache.prefix').'theme', function () {
-                return Themes::where('is_default', true)->first();
+            if (Cache::has(config('cache.prefix').'theme')) {
+                $theme = Cache::get(config('cache.prefix').'theme');
+            } else {
+                $theme = Cache::rememberForever(config('cache.prefix').'theme', function () {
+                    return Themes::where('is_default', true)->first();
+                });
+            }
+            App::singleton('theme', function () use ($theme) {
+                return $theme;
             });
-        }
-        App::singleton('theme', function () use ($theme) {
-            return $theme;
-        });
 
-        if (Cache::has(config('cache.prefix').'ad_settings')) {
-            $ad_settings = Cache::get(config('cache.prefix').'ad_settings');
-        } else {
-            $ad_settings = Cache::rememberForever(config('cache.prefix').'ad_settings', function () {
-                return AdvertiseSettings::first();
+            if (Cache::has(config('cache.prefix').'ad_settings')) {
+                $ad_settings = Cache::get(config('cache.prefix').'ad_settings');
+            } else {
+                $ad_settings = Cache::rememberForever(config('cache.prefix').'ad_settings', function () {
+                    return AdvertiseSettings::first();
+                });
+            }
+            App::singleton('ad_settings', function () use ($ad_settings) {
+                return $ad_settings;
             });
-        }
-        App::singleton('ad_settings', function () use ($ad_settings) {
-            return $ad_settings;
-        });
 
-        if (Cache::has(config('cache.prefix').'analytic_settings')) {
-            $analytic_settings = Cache::get(config('cache.prefix').'analytic_settings');
-        } else {
-            $analytic_settings = Cache::rememberForever(config('cache.prefix').'analytic_settings', function () {
-                return AnalyticsSettings::first();
+            if (Cache::has(config('cache.prefix').'analytic_settings')) {
+                $analytic_settings = Cache::get(config('cache.prefix').'analytic_settings');
+            } else {
+                $analytic_settings = Cache::rememberForever(config('cache.prefix').'analytic_settings', function () {
+                    return AnalyticsSettings::first();
+                });
+            }
+            App::singleton('analytic_settings', function () use ($analytic_settings) {
+                return $analytic_settings;
             });
-        }
-        App::singleton('analytic_settings', function () use ($analytic_settings) {
-            return $analytic_settings;
-        });
 
-        if (Cache::has(config('cache.prefix').'social_networks')) {
-            $social_networks = Cache::get(config('cache.prefix').'social_networks');
-        } else {
-            $social_networks = Cache::rememberForever(config('cache.prefix').'social_networks', function () {
-                return SocialNetworks::where('type', 'website')->first();
+            if (Cache::has(config('cache.prefix').'social_networks')) {
+                $social_networks = Cache::get(config('cache.prefix').'social_networks');
+            } else {
+                $social_networks = Cache::rememberForever(config('cache.prefix').'social_networks', function () {
+                    return SocialNetworks::where('type', 'website')->first();
+                });
+            }
+            App::singleton('social_networks', function () use ($social_networks) {
+                return $social_networks;
             });
-        }
-        App::singleton('social_networks', function () use ($social_networks) {
-            return $social_networks;
-        });
 
-        if (Cache::has(config('cache.prefix').'social_settings')) {
-            $social_settings = Cache::get(config('cache.prefix').'social_settings');
-        } else {
-            $social_settings = Cache::rememberForever(config('cache.prefix').'social_settings', function () {
-                return SocialSettings::first();
+            if (Cache::has(config('cache.prefix').'social_settings')) {
+                $social_settings = Cache::get(config('cache.prefix').'social_settings');
+            } else {
+                $social_settings = Cache::rememberForever(config('cache.prefix').'social_settings', function () {
+                    return SocialSettings::first();
+                });
+            }
+            App::singleton('social_settings', function () use ($social_settings) {
+                return $social_settings;
             });
-        }
-        App::singleton('social_settings', function () use ($social_settings) {
-            return $social_settings;
-        });
 
-        View::share('general_settings', $general_settings);
-        View::share('languages', $languages);
-        View::share('default_language', $default_language);
-        View::share('theme', $theme);
-        View::share('ad_settings', $ad_settings);
-        View::share('analytic_settings', $analytic_settings);
-        View::share('social_networks', $social_networks);
-        View::share('social_settings', $social_settings);
+            View::share('general_settings', $general_settings);
+            View::share('languages', $languages);
+            View::share('default_language', $default_language);
+            View::share('theme', $theme);
+            View::share('ad_settings', $ad_settings);
+            View::share('analytic_settings', $analytic_settings);
+            View::share('social_networks', $social_networks);
+            View::share('social_settings', $social_settings);
+        }
     }
 }
