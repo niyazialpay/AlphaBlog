cafed4a975c6e9a6a54f40385baf0da3d74e56bd
Muhammed Niyazi Alpay <muhammed@niyazialpay.com>
2024-10-12 11:03:32 +0300

    update

--- resources/views/panel/post/category/partials/category_row.blade.php
+++ resources/views/panel/post/category/partials/category_row.blade.php
@@ -3,8 +3,8 @@
         @if($level > 0)
             <i class="fa-sharp-duotone fa-solid fa-arrow-turn-down-right"></i>
         @endif
-        @if($category->getFirstMediaUrl('categories', 'thumb'))
-            <img src="{{ $category->getFirstMediaUrl('categories', 'thumb') }}" alt="{{ $category->name }}"
+        @if($categories->getFirstMediaUrl('categories', 'thumb'))
+            <img src="{{ $categories->getFirstMediaUrl('categories', 'thumb') }}" alt="{{ $categories->name }}"
                  class="img-fluid" width="50">
         @endif
     </td>
@@ -12,16 +12,16 @@
         @if($level > 0)
             <i class="fa-sharp-duotone fa-solid fa-arrow-turn-down-right"></i>
         @endif
-        {{ $category->name }}
+        {{ $categories->name }}
     </td>
     <td>
-        <a href="{{ route('admin.categories', $category) }}"
+        <a href="{{ route('admin.categories', $categories) }}"
            class="btn btn-sm btn-primary mx-1"
            data-bs-toggle="tooltip" data-bs-placement="top"
            title="@lang('general.edit')">
             <i class="fa fa-edit"></i>
         </a>
-        <a href="javascript:Delete('{{ $category->id }}')"
+        <a href="javascript:Delete('{{ $categories->id }}')"
            class="btn btn-sm btn-danger mx-1"
            data-bs-toggle="tooltip" data-bs-placement="top"
            title="@lang('general.delete')">
@@ -30,7 +30,7 @@
     </td>
 </tr>
 
-@forelse($category->children as $child)
-    @include('panel.post.category.partials.category_row', ['category' => $child, 'level' => $level + 1])
+@forelse($categories->children as $child)
+    @include('panel.post.category.partials.category_row', ['categories' => $child, 'level' => $level + 1])
 @empty
 @endforelse
