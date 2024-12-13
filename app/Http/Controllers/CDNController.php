4d55c196deadf6aee6fec90412d62d0aa9c0ded6
Muhammed Niyazi Alpay <muhammed@niyazialpay.com>
2024-12-13 23:52:45 +0300

    update

--- app/Http/Controllers/CDNController.php
+++ app/Http/Controllers/CDNController.php
@@ -6,9 +6,12 @@ use Illuminate\Http\Request;
 
 class CDNController extends Controller
 {
-    public function index($any){
+    public function index($any=null){
+        if($any == null){
+            abort(404);
+        }
         $path = public_path($any);
-        
+
         if (file_exists($path) && is_file($path)) {
             return response()->file($path);
         }
