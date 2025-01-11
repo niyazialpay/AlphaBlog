e8e82241807645491a5c0fde779abdcd89f1831d
Muhammed Niyazi Alpay <muhammed@niyazialpay.com>
2025-01-11 17:43:32 +0300

    update

--- resources/views/panel/logs/index.blade.php
+++ resources/views/panel/logs/index.blade.php
@@ -36,11 +36,22 @@
     </div>
 @endsection
 @section('script')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.3/css/responsive.bootstrap5.css">
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+    <style>
+        .table-responsive{
+            overflow: hidden !important;
+        }
+        table.dataTable.dtr-inline.collapsed > tbody > tr > td.dtr-control:before, table.dataTable.dtr-inline.collapsed > tbody > tr > th.dtr-control:before {
+            margin-right: 0.5em;
+            display: inline-block;
+            box-sizing: border-box;
+            content: "";
+            border-top: 5px solid transparent;
+            border-left: 10px solid rgba(0, 0, 0, 0.5);
+            border-bottom: 5px solid transparent;
+            border-right: 0px solid transparent;
+        }
+    </style>
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.bootstrap5.min.css">
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.js"></script>
     <script src="https://cdn.datatables.net/responsive/3.0.3/js/dataTables.responsive.js"></script>
