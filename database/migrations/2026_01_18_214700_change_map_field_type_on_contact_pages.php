97fb8e5d90e6f6d8ae91ec3f559ff785abfe0681
Muhammed Niyazi Alpay <muhammed@niyazialpay.com>
2026-01-18 21:49:10 +0300

    update

---
+++ database/migrations/2026_01_18_214700_change_map_field_type_on_contact_pages.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('contact_pages', function(Blueprint $table){
+            $table->text('maps');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('contact_pages', function(Blueprint $table){
+            $table->string('maps');
+        });
+    }
+};
